5eff26cff735f4a9eb754ece3afa26500695f44d
mu-tech8 <magobe.theword@gmail.com>
2022-05-26 21:50:04 +0900

    modify Layout file

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -12,10 +12,10 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="../../css/app.css">
+    <link rel="stylesheet" href="{{ asset('/css/app.css')}}">
 
     <!-- Scripts -->
-    <script src="../../js/app.js" defer></script>
+    <script src="{{ asset('/js/app.js') }}" defer></script>
     <!-- Flowbite -->
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.3.4/dist/flowbite.min.css" />
 
